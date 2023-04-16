ec927e93c6999708f0eac3356fa5375ee017b2d0
Ritesh <shenderitesh3@gmail.com>
2023-04-16 16:18:29 +0530

    Add program_3.py

--- program_3.py
+++ program_3.py
@@ -1,5 +1,1 @@
 # accept a string and a char from user and check if char is present in str
-
-user_string = input("string: ")
-user_character = input("Character: ")
-print(user_string.find(user_character))
